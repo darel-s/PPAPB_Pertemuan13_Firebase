c725a0f44e1d77481b38f51a69de882def1fb302
Darriel Markerizal <darrielmarkerizal@gmail.com>
2023-11-23 09:04:11 +0700

    firebase and dataclass

--- app/src/main/java/com/example/pertemuan13_firebase/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan13_firebase/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.pertemuan13_firebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.MutableLiveData
+import com.example.pertemuan13_firebase.databinding.ActivityMainBinding
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val firestore = FirebaseFirestore.getInstance()
+    private var updateId = ""
+    private val budgetListLiveData : MutableLiveData<List<Budget>>
+    by lazy {
+        MutableLiveData<List<Budget>>()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
 }
